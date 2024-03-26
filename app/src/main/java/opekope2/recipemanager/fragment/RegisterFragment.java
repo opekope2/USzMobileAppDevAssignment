a48ffa27a1b56d9b0c1e4b19931018bb27cb2e68
opekope2 <opekope2@gmail.com>
2024-03-26 18:31:30 +0100

    Add register fragment

---
+++ app/src/main/java/opekope2/recipemanager/fragment/RegisterFragment.java
@@ -0,0 +1,22 @@
+package opekope2.recipemanager.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import opekope2.recipemanager.R;
+
+public class RegisterFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
